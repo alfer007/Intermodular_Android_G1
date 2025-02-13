d6f7c272daec66fea9b711febb61afc3347c639d
AlePuche <alepucheperez@gmail.com>
2025-02-13 17:51:49 +0100

    Logo redondeado

--- app/src/main/java/com/arturotorralbo/aplicacionintermodulargrupo1/Home/components/HeaderSection.kt
+++ app/src/main/java/com/arturotorralbo/aplicacionintermodulargrupo1/Home/components/HeaderSection.kt
@@ -1,11 +1,15 @@
 package com.arturotorralbo.aplicacionintermodulargrupo1.home.components
 
 import android.content.Context
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AppRegistration
 import androidx.compose.material.icons.filled.GpsFixed
@@ -16,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
@@ -26,6 +31,7 @@ import com.arturotorralbo.aplicacionintermodulargrupo1.core.navigation.Login
 import com.arturotorralbo.aplicacionintermodulargrupo1.core.navigation.Profile
 import com.arturotorralbo.aplicacionintermodulargrupo1.core.navigation.Register
 import com.arturotorralbo.aplicacionintermodulargrupo1.core.utils.TokenManager
+import com.arturotorralbo.aplicacionintermodulargrupo1.ui.theme.primaryColorBlue
 
 @Composable
 fun HeaderSection(navController: NavController, context: Context) {
@@ -41,8 +47,13 @@ fun HeaderSection(navController: NavController, context: Context) {
         AsyncImage(
             model = "http://10.0.2.2:3000/images/hotellogoestrellas.png",
             contentDescription = "Logo Hotel",
-            modifier = Modifier.size(200.dp),
-            contentScale = ContentScale.Fit
+            modifier = Modifier
+                .height(60.dp)
+                .width(150.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .border(3.dp, primaryColorBlue, RoundedCornerShape(20.dp))
+                .padding(3.dp),
+            contentScale = ContentScale.FillBounds
         )
         Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
             IconButton(onClick = { openGoogleMaps(context) }) {
